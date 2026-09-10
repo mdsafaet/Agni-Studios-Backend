ad88e4080f1c975372336f6b5276adaf83ecb992
mdsafaet <mdsafaet84@gmail.com>
2026-09-10 11:19:15 +0600

    chnage password

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile(isSimple: false)
             ->darkMode(false)
             ->defaultThemeMode(ThemeMode::Light)
             ->colors([
